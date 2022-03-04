94c26a5cd52fcac79fb40071bb64b882af5b1bee
prophet7821 <samridhadas782001@gmail.com>
2022-03-04 22:46:14 +0530

    Carousel & Viewer added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,25 @@
 import styled from "styled-components";
 import ImgSlider from './ImgSlider';
-
+import Viewers from './Viewers';
 const Home = (props) => {
   return (
   <Container>
     <ImgSlider/>
+    <Viewers/>
   </Container>);
 };
 
 const Container = styled.main`
 position:relative;
-
-min-height:calc(100vh-250px);
+min-height: calc(100vh-250px);
 overflow-x:hidden;
 display:block;
 top:72px;
-padding:calc(1.5vw+5px);
+padding:0 calc(3.5vw+5px);
 
 &:after{
-  background:url('/images/home-background.png') center center /cover no-repeat fixed;
+  background:url('/images/home-background.png') center center / cover
+   no-repeat fixed;
   content:'';
   position:absolute;
   inset:0px;
