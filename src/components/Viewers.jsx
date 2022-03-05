88129b13a7a071b8d89aefa575f649afce87b304
prophet7821 <samridhadas782001@gmail.com>
2022-03-05 12:00:41 +0530

    Added Recommendations and multiple components & connected to db

--- src/components/Viewers.jsx
+++ src/components/Viewers.jsx
@@ -47,7 +47,6 @@ const Container = styled.div`
   grid-gap: 25px;
   gap: 25px;
   grid-template-columns: repeat(5, minmax(0, 1fr));
-
   @media (max-width: 768px) {
     grid-template-columns: repeat(1, minmax(0, 1fr));
   }
@@ -63,7 +62,6 @@ const Wrap = styled.div`
   position: relative;
   transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
   border: 3px solid rgba(249, 249, 249, 0.1);
-
   img {
     inset: 0px;
     display: block;
@@ -76,7 +74,6 @@ const Wrap = styled.div`
     z-index: 1;
     top: 0;
   }
-
   video {
     width: 100%;
     height: 100%;
@@ -85,14 +82,11 @@ const Wrap = styled.div`
     opacity: 0;
     z-index: 0;
   }
-
   &:hover {
     box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
-
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
-
     video {
       opacity: 1;
     }
